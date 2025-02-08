566b02977ada880aa72b2bc47e74796cea1f91a7
mhalr <ryo97321@gmail.com>
2025-02-08 13:51:10 +0900

    step4:エラーメッセージを改良

--- 9cc.c
+++ 9cc.c
@@ -25,6 +25,23 @@ struct Token {
 // 現在着目しているトークン
 Token *token;
 
+// 入力プログラム
+char *user_input;
+
+// エラー箇所を報告する
+void error_at(char *loc, char *fmt, ...) {
+	va_list ap;
+	va_start(ap, fmt);
+
+	int pos = loc - user_input;
+	fprintf(stderr, "%s\n", user_input);
+	fprintf(stderr, "%*s", pos, " "); // pos個の空白を出力
+	fprintf(stderr, "^ ");
+	vfprintf(stderr, fmt, ap);
+	fprintf(stderr, "\n");
+	exit(1);
+}
+
 // エラーを報告するための関数
 // printfと同じ引数を取る
 void error(char *fmt, ...) {
@@ -56,7 +73,7 @@ void expect(char op) {
 // それ以外の場合にはエラーを報告する。
 int expect_number() {
 	if (token->kind != TK_NUM)
-		error("数ではありません");
+		error_at(token->str, "数ではありません");
 	int val = token->val;
 	token = token->next;
 	return val;
@@ -106,14 +123,18 @@ Token *tokenize(char *p) {
 	return head.next;
 }
 
+
 int main(int argc, char **argv) {
 	if (argc != 2) {
 		fprintf(stderr, "引数の個数が正しくありません\n");
 		return 1;
 	}
 
+	// 入力プログラム
+	user_input = argv[1];
+
 	// トークナイズする
-	token = tokenize(argv[1]);
+	token = tokenize(user_input);
 
 	// アセンブリの前半部分を出力
 	printf(".intel_syntax noprefix\n");
